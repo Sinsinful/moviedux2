4eb2d2b1f66cf1537effc6a9ac2dbac34aa18071
Sinsinful <sin_1210@hotmail.com>
2025-07-21 20:30:46 +0100

    Added router, navbar, watchlist, refactored movies state and driled props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,66 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchList = (movieId) => {
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  watchlist={watchlist}
+                  movies={movies}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            ></Route>
+            <Route
+              path="watchlist"
+              element={
+                <Watchlist
+                  watchlist={watchlist}
+                  movies={movies}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>
